0f87a5be6b37b74970ceee28e0cafc138b6034aa
katskoru <katskoru@gmail.com>
2022-01-29 21:47:46 +0100

    elevatedButtonSize

--- lib/screens/AnimatedContainer.dart
+++ lib/screens/AnimatedContainer.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:project_from_figma/Widgets/text_widget.dart';
 
 class MyAnCont extends StatefulWidget {
   MyAnCont({Key? key}) : super(key: key);
@@ -25,46 +26,47 @@ class _MyAnContState extends State<MyAnCont> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.center,
-            ),
-            Container(
-              height: 20.0,
-            ),
-            AnimatedContainer(
-              duration: const Duration(milliseconds: 500),
-              color: list_of_colors[index1],
-              width: width.toDouble(),
-              height: height.toDouble(),
-            ),
-            Container(
-              height: 20.0,
-            ),
-            Container(
-              height: 70.0,
-              width: 200.0,
-              child: ElevatedButton(
-                  child: Text("Click me"),
-                  style: ElevatedButton.styleFrom(
-                      primary: _isInitialValue ? Colors.green : Colors.blue,
-                      shape: StadiumBorder(),
-                      shadowColor: Colors.grey[900],
-                      elevation: 10.0),
-                  onPressed: () {
-                    setState(() {
-                      _isInitialValue = !_isInitialValue;
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.center,
+          ),
+          Container(
+            height: 20.0,
+          ),
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 500),
+            color: list_of_colors[index1],
+            width: width.toDouble(),
+            height: height.toDouble(),
+          ),
+          Container(
+            height: 20.0,
+          ),
+          Container(
+            height: 70.0,
+            width: 300.0,
+            child: ElevatedButton(
+                child: MyTextWidget(
+                  text: "Click me",
+                  size: 30.0,
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: _isInitialValue ? Colors.green : Colors.blue,
+                    shape: StadiumBorder(),
+                    shadowColor: Colors.grey[900],
+                    elevation: 10.0),
+                onPressed: () {
+                  setState(() {
+                    _isInitialValue = !_isInitialValue;
 
-                      width = Random().nextInt(300);
-                      height = Random().nextInt(300);
-                      index1 = Random().nextInt(list_of_colors.length - 1);
-                    });
-                  }),
-            )
-          ],
-        ),
+                    width = Random().nextInt(300);
+                    height = Random().nextInt(300);
+                    index1 = Random().nextInt(list_of_colors.length - 1);
+                  });
+                }),
+          )
+        ],
       ),
     );
   }
